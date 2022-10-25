f570be5b5f8bf9feb82ad877f20ce6661db4edc9
Khashrul Alam <khashrul.alam@sslwireless.com>
2022-10-25 11:00:39 +0600

    Migration Update

--- database/migrations/2022_10_20_042258_add_new_column_to_users_table.php
+++ database/migrations/2022_10_20_042258_add_new_column_to_users_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('user_type')->default(null);
+            $table->enum('user_type', [
+                'admin', 'client', 'user'
+            ])->default('admin');
         });
     }
 
