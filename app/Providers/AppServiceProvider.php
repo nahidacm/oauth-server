94a807b7be5344fecaa2c73eb44366b6410d75f7
Khashrul Alam <khashrul.alam@sslwireless.com>
2022-10-25 10:49:26 +0600

    Force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,9 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Paginator::useBootstrap();
-        if ($this->app->environment('production')) {
+        if (env('APP_ENV') !== 'local') {
             URL::forceScheme('https');
         }
+
+        Paginator::useBootstrap();
     }
 }
